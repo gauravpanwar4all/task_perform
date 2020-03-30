78cca78c915e89d815f021fbaa5100b6ef1cd831
gauravpanwar4all <62298353+gauravpanwar4all@users.noreply.github.com>
2020-03-30 14:00:02 +0530

    task for job

---
+++ precache-manifest.f6c1e68e894660765a1e6e2d2acdf5e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e1f61e6a568997bf9486e171918af1d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b8878e630d24896c6eee",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "f7bceac232c2173f96f9",
+    "url": "/static/css/main.c5b21a58.chunk.css"
+  },
+  {
+    "revision": "b8878e630d24896c6eee",
+    "url": "/static/js/2.a4dec3aa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.a4dec3aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7bceac232c2173f96f9",
+    "url": "/static/js/main.f4a8853d.chunk.js"
+  },
+  {
+    "revision": "27151745f3f65a1762eb",
+    "url": "/static/js/runtime-main.35f62e43.js"
+  }
+]);
